3909ede631897cf28b25827239d668ad1cb2269a
philip-mudenyo <philip.mudenyo@southwell.io>
2018-07-31 17:31:01 +0300

    read credentials from file

--- chatbot/service.sh
+++ chatbot/service.sh
@@ -1,15 +1,118 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-. /go/src/github.com/mudphilo/chat/chatbot/ticktick.sh
+### BEGIN INIT INFO
+# Provides:          chatbot
+# Required-Start:    $local_fs $network $named $time $syslog
+# Required-Stop:     $local_fs $network $named $time $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Description:       Script to run chatbot as a service
+### END INIT INFO
 
+SCRIPT_NAME="/go/src/github.com/mudphilo/chat/chatbot/chatbot.py"
+RUNAS=root
+NAME="chatbot"
+
+PIDFILE="/var/run/$NAME.pid"
+LOGFILE="/var/log/$NAME/daemon.log"
+
+TICKTICK="/go/src/github.com/mudphilo/chat/chatbot/ticktick.sh"
+CREDENTIALS ="/go/src/github.com/mudphilo/chat/chatbot/cookie.json"
+
+echo "loading ticktick library"
+. $TICKTICK
+
+echo "load credentials file"
 # File
-DATA=`cat /go/src/github.com/mudphilo/chat/chatbot/cookie.json`
-
+DATA=`cat $CREDENTIALS`
+echo "parse credentials file"
 tickParse "$DATA"
 
 schema=``schema``
 secret=``secret``
 
-echo "$schema"
+echo "got secret $secret and schema $schema"
 
-echo "$secret"
+SCRIPT="python3.6 $SCRIPT_NAME --login-$schema=$secret"
+
+start() {
+  if [ -f $PIDFILE ] && [ -s $PIDFILE ] && kill -0 $(cat $PIDFILE); then
+    echo 'Service already running' >&2
+    return 1
+  fi
+  echo 'Starting service?~@?' >&2
+  local CMD="$SCRIPT &> \"$LOGFILE\" & echo \$!"
+  su -c "$CMD" $RUNAS > "$PIDFILE"
+ # Try with this command line instead of above if not workable
+ # su -s /bin/sh $RUNAS -c "$CMD" > "$PIDFILE"
+
+  sleep 2
+  PID=$(cat $PIDFILE)
+    if pgrep -u $RUNAS -f $NAME > /dev/null
+    then
+      echo "$NAME is now running, the PID is $PID"
+    else
+      echo ''
+      echo "Error! Could not start $NAME!"
+    fi
+}
+
+stop() {
+  if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
+    echo 'Service not running' >&2
+    return 1
+  fi
+  echo 'Stopping service?~@?' >&2
+  kill -15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
+  echo 'Service stopped' >&2
+}
+
+uninstall() {
+  echo -n "Are you really sure you want to uninstall this service? That cannot be undone. [yes|No] "
+  local SURE
+  read SURE
+  if [ "$SURE" = "yes" ]; then
+    stop
+    rm -f "$PIDFILE"
+    echo "Notice: log file was not removed: $LOGFILE" >&2
+    update-rc.d -f $NAME remove
+    rm -fv "$0"
+  else
+    echo "Abort!"
+  fi
+}
+
+status() {
+    printf "%-50s" "Checking $NAME ..."
+    if [ -f $PIDFILE ] && [ -s $PIDFILE ]; then
+        PID=$(cat $PIDFILE)
+            if [ -z "$(ps axf | grep ${PID} | grep -v grep)" ]; then
+                printf "%s\n" "The process appears to be dead but pidfile still exists"
+            else
+                echo "Running, the PID is $PID"
+            fi
+    else
+        printf "%s\n" "Service not running"
+    fi
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  status)
+    status
+    ;;
+  uninstall)
+    uninstall
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|status|restart|uninstall}"
+esac
